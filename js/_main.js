a803eec6557d9bca90bcbdf3f7e5fd02bdda2a73
ekranac <besal.ziga@gmail.com>
2014-04-27 15:31:21 +0200

    better version :)

--- js/_main.js
+++ js/_main.js
@@ -1,6 +1,9 @@
 function paralax()
 {
-    var prlx = document.getElementById("prlx");
-    prlx.style.top = - (window.pageYOffset/9) + "px" //This line gets the whole thing done
+    var prlx1 = document.getElementById("prlx");
+    prlx1.style.top = - (window.pageYOffset/9) + "px" //This line gets the whole thing done
+    
+    var prlx2 = document.getElementById("header")
+    prlx2.style.top = -(window.pageYOffset/4) + "px"
 }
 window.addEventListener("scroll", paralax, false) //Triggers a function on scroll event
